85f23ee3c3e4fb296c883bcf27a0e9cb3ba8037f
semenov <semenov>
2024-10-06 20:09:25 +0400

    41 Joom парсинг (config)

--- src/constuctor.module/constuctor.interface.ts
+++ src/constuctor.module/constuctor.interface.ts
@@ -3,12 +3,25 @@ import { StoreConfigElement } from "@src/store.module/store.module";
 import { StoreConfig } from "@src/store.module/store.module"
 
 export enum CommandAction {
-    CONNECTION_DATABASE = "connectionDatabase",
-    SQL_CALL = "sql",
-    FILE_CONFIG = "fileConfig",
-    FILE_READ = "fileRead",
-    FILE_WRITE = "fileWrite",
-    MAPPING_JSON = "mappingJson"
+    CONNECTION_DATABASE = "connectionDatabase", // подключение к бд
+    SQL_CALL = "sql", // вызов sql
+
+    FILE_CONFIG = "fileConfig", // ссылка на файл конфиг 
+    FILE_READ = "fileRead", // чтения файла
+    FILE_WRITE = "fileWrite", // запись в файл
+    DIRECTORY_FILE = "directoryFile", // выдать пути файлов в каталоге
+
+    MAPPING_JSON = "mappingJson", // из 1 json в др. json
+
+    INIT_VAR = "initVar", // создать(иницилизировать) переменную
+    
+    FOR = "for", // прогнать в цикле что либо
+
+    CONVERT_IN_DOM = "convertInDom", // команда из строки в dom-element
+    FIND_ELEMENT_HTML_ALL = "findElementHtmlAll", // найти в html все element по selector
+    GET_INNER_HTML = "getInnerHtml", // получить текст и html с dom-element
+    GET_ATR_HTML = "getAtrHtml" // получить атрибут с dom-element
+
 }
 
 export enum CommandResultOperator {
@@ -21,6 +34,10 @@ export interface Command {
     result: CommandResultOperator,
     name: string | string[];
     comment: string | null
+}
+
+export interface CommandInitVar extends Command {
+    initVar: StoreConfigElement
 }
 
 export interface CommandConnectionDatabase extends Command {
@@ -40,14 +57,16 @@ export interface CommandFileRead extends Command {
     path: StoreConfigElement;
 }
 
+export interface CommandFileConfig extends CommandFileRead { }
+
+export interface CommandDirectoryFile extends CommandFileRead { }
+
 export interface CommandFileWrite extends Command {
     fileWrite: {
         path: StoreConfigElement;
         data: StoreConfigElement;
     }
 }
-
-export interface CommandFileConfig extends CommandFileRead { }
 
 export interface CommandMappigJson extends Command {
     mappingJson: {
--- src/constuctor.module/constuctor.service.ts
+++ src/constuctor.module/constuctor.service.ts
@@ -1,4 +1,4 @@
-import { Command, CommandAction, CommandConnectionDatabase, CommandFileConfig, CommandFileRead, CommandFileWrite, CommandMappigJson, CommandSql } from "@src/constuctor.module/constuctor.interface";
+import { Command, CommandAction, CommandConnectionDatabase, CommandDirectoryFile, CommandFileConfig, CommandFileRead, CommandFileWrite, CommandInitVar, CommandMappigJson, CommandSql } from "@src/constuctor.module/constuctor.interface";
 
 import { bdService } from "@src/bd.module/bd.module";
 import { fileService } from "@src/file.module/file.module";
@@ -17,37 +17,47 @@ class ConstuctorService {
         for (const [index, command] of commandList.entries()) {
             let resultCommand = null;
             switch (command.action) {
-                case CommandAction.CONNECTION_DATABASE:
+                case CommandAction.FILE_CONFIG: // файл конфиг
+                    await this.convertFileConig(command as CommandFileConfig, commandList, index);
+                    break;
+
+                case CommandAction.CONNECTION_DATABASE: // подключение к бд
                     const commandConnectionDatabase = command as CommandConnectionDatabase;
                     resultCommand = bdService.connectionDatabase(commandConnectionDatabase.connection);
                     break;
 
-                case CommandAction.FILE_CONFIG:
-                    await this.convertFileConig(command as CommandFileConfig, commandList, index);
-                    break;
-
-                case CommandAction.SQL_CALL:
+                case CommandAction.SQL_CALL: // вызов sql функции
                     const commandSql = command as CommandSql;
                     resultCommand = await bdService.sqlCall(commandSql);
                     break;
 
-                case CommandAction.FILE_READ:
+                case CommandAction.FILE_READ: // чтения файла
                     const commandFileRead = command as CommandFileRead;
                     const pathFileRead = storeConfigService.getElementStoreConfigConstructor(commandFileRead.path) as string;
                     resultCommand = await fileService.readFile(pathFileRead);
                     break;
 
-                case CommandAction.FILE_WRITE:
+                case CommandAction.FILE_WRITE: // запись файла
                     const commandFileWrite = command as CommandFileWrite;
                     const pathFileWrite = storeConfigService.getElementStoreConfigConstructor(commandFileWrite.fileWrite.path);
                     const dataFileWrite = storeConfigService.getElementStoreConfigConstructor(commandFileWrite.fileWrite.data);
                     await fileService.writeFile(pathFileWrite, dataFileWrite);
                     break;
 
-                case CommandAction.MAPPING_JSON:
+                case CommandAction.DIRECTORY_FILE: // чтения путей в файлов в каталоге
+                    const commandDirectoryFile = command as CommandDirectoryFile;
+                    const pathDirectoryFile = storeConfigService.getElementStoreConfigConstructor(commandDirectoryFile.path);
+                    resultCommand = await fileService.directoryFile(pathDirectoryFile);
+
+                case CommandAction.MAPPING_JSON: // парсинг json в др формат json
                     const commandMappingJson = command as CommandMappigJson;
                     const dataJsonMapping = storeConfigService.getElementStoreConfigConstructor(commandMappingJson.mappingJson.json);
                     resultCommand = jsonService.mappingJson(dataJsonMapping, commandMappingJson.mappingJson.schema);
+                    break;
+
+                case CommandAction.INIT_VAR: // иницилизация переменной
+                    const commandInitVar = command as CommandInitVar;
+                    resultCommand = storeConfigService.getElementStoreConfigConstructor(commandInitVar.initVar);
                     break;
             }
             storeConfigService.setStore(command.name, resultCommand, command.result);
