82e7a53effb380d0446299f70b65cc382b1bedfe
semenov <semenov>
2024-10-17 22:46:28 +0400

    43 LoggerService

--- src/logger.module/logger.service.ts
+++ src/logger.module/logger.service.ts
@@ -2,17 +2,6 @@ import { createLogger, format, transports } from "winston"
 
 export const loggerService = createLogger({
     transports: [
-        new transports.Console({
-            format: format.combine(
-                format.colorize(),
-                format.timestamp(),
-                format.printf(({ timestamp, level, message, metadata }) => {
-                    return `[${timestamp}] ${level}: ${message}. ${JSON.stringify(
-                        metadata
-                    )}`;
-                })
-            ),
-        }),
         new transports.File({
             dirname: ".log",
             filename: "log.json",
