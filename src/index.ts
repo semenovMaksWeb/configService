4a9a0d3e734eccd4ac8338cac3de453daa550f39
semenov <semenov>
2024-11-08 22:15:35 +0400

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import configTest from "../config/joom/product/index.json"
+import configTest from "../../configMedia/config/joom/categories/index.json"
 
 import { Command, constuctorService } from "./constuctor.module/constuctor.module";
 import { storeService } from "./store.module/store.service";
@@ -6,6 +6,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 (async () => {
-    await constuctorService.runConfig(configTest as Command[], { isDowload: false, parsing: false, db: true });
+    await constuctorService.runConfig(configTest as Command[], { isDowload: false, parsing: false, db: false });
     console.log(storeService.getAllStore());
 })();
