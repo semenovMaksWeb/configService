40c01ef437a2a89d85c297f2ebf9992862cc7255
semenov <semenov>
2024-11-16 20:52:30 +0400

    тесты с зависимостями как пакет

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import configTest from "../../configMedia/config/joom/index.json"
+import { constuctorService } from "./constuctor.module/constuctor.service";
 
-import { Command, constuctorService } from "./constuctor.module/constuctor.module";
-import { storeService } from "./store.module/store.service";
-const dotenv = require('dotenv');
-dotenv.config();
-
-(async () => {
-    await constuctorService.runConfig(configTest as Command[], {
-        categories: {
-            webHtml: false,
-            parsing: false,
-            db: true,
-            isDowload: false,           
-        }
-    });
-    console.log(storeService.getAllStore());
-})();
+export const configCore = {
+    constuctorService,
+}
