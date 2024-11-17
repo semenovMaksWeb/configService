e2c7f141758e2325662363b138a4fbd22d39755b
semenov <semenov>
2024-11-17 12:41:36 +0400

    import return index

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { constuctorService } from "@src/constuctor.module/constuctor.module";
+import { pgService } from "@src/db.module/pg/pg.service";
+import { fileService } from "@src/file.module/file.module";
+import { htmlService } from "@src/html.module/html.module"
+import { webService } from "@src/web.module/web.module";
+import { convertService } from "@src/libs.module/libs.module";
+
+export const configCore = {
+    constuctorService,
+    pgService,
+    fileService,
+    htmlService,
+    webService,
+    convertService
+}
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { constuctorService } from "./constuctor.module/constuctor.service";
-
-export const configCore = {
-    constuctorService,
-}
